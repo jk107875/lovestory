bae3812fc370ac7041cfd63b05c6bc26a1438087
jk107875 <144523849+jk107875@users.noreply.github.com>
2024-04-21 13:20:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -51,12 +51,13 @@ def get_weather(region):
     weather = '白天'+response['daily'][0]["textDay"]+'，'+'傍晚'+response['daily'][0]["textDay"]
     # 当前温度
     temp = response['daily'][0]["tempMin"]+ u"\N{DEGREE SIGN}" + "C"+'—'+response['daily'][0]["tempMax"]+ u"\N{DEGREE SIGN}" + "C"
-    if int(response['daily'][0]["tempMin"]) <= 18:
-        xigua = "天气变凉啦，多穿点衣服哦~"
+    if int(response['daily'][0]["tempMin"]) <= 16:
+        temp = temp + "早上凉快，记得多穿点衣服哦~"
     else:
-        xigua = "今天又是很想你的一天~"
+        temp = temp + "今天的天气也很适合想我哦~"
     # 风向
     wind_dir = response['daily'][0]["windDirDay"]
+    xigua =""
     return weather, temp, wind_dir, xigua
  
 #--------关注微信公众号：繁星资源，更多资源等你拿----------
